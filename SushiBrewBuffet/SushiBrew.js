b87b460bea1f6b1209ddf289f960aaea83f78037
noedig101 <149635050+noedig101@users.noreply.github.com>
2026-09-19 22:15:07 -0400

    Update SushiBrew.js

--- SushiBrewBuffet/SushiBrew.js
+++ SushiBrewBuffet/SushiBrew.js
@@ -7,7 +7,9 @@ function incPlayer(){
 
 function decPlayer(){
   var player = Number(document.getElementById("player").innerHTML)
+  println(player);
   if(player > 5){
+    println("test good");
     document.getElementById("player").innerHTML = (player-1).toString();
   }
 };
