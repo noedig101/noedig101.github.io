7bdf7e30a3728408530bc67df7d6a5767ee71d49
noedig101 <149635050+noedig101@users.noreply.github.com>
2026-09-21 14:45:12 -0400

    Update SushiBrew.js

--- SushiBrewTest/SushiBrew.js
+++ SushiBrewTest/SushiBrew.js
@@ -33,14 +33,13 @@ var choices = [];
 var bluffs = [];
 function isUnique(char){
   window.alert(choices);
-  for(j=0;j++;j<choices.length){
+  for(j=0;j++;j>=choices.length){
     if (choices[j].name == char.name){
       return false;
     }
   }
   window.alert(bluffs);
-  for(j=0;j++;j<bluffs.length){
-    window.alert(bluffs[j]+" "+char.name);
+  for(j=0;j++;j>=bluffs.length){
     if (bluffs[j] == char.name){
       return false;
     }
